2745577daf1988c64bfd6f544735ea308b55bfaa
Hamid Farmani <hamidfarmani1@gmail.com>
2023-07-22 09:40:05 +0200

    feat: show words list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
-export default function Home() {
-  return <h1>Home</h1>;
+import { prisma } from "@/db";
+
+export default async function Home() {
+  const words = await prisma.word.findMany();
+
+  return (
+    <>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">WordWise</h1>
+      </header>
+
+      <ul className="pl-4">
+        {words.map((word) => (
+          <li key={word.id} className="mb-4">
+            <h2 className="text-xl">{word.word}</h2>
+            <p className="text-gray-400">{word.definition}</p>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
