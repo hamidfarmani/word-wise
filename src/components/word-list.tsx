78138a2ca82c1c32985a928ece916e681f871051
Hamid Farmani <hamidfarmani1@gmail.com>
2024-09-30 19:12:45 +0200

    fix: check if words exist

--- src/components/word-list.tsx
+++ src/components/word-list.tsx
@@ -6,7 +6,7 @@ export default async function WordList() {
 
   return (
     <div className="grid grid-cols-4 gap-3">
-      {words.length > 0 ? (
+      {words && words.length > 0 ? (
         words.map((word) => <WordCard key={word.id} word={word} />)
       ) : (
         <div className="col-span-3">No words found</div>
